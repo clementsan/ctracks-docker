3a9afa6917378d3a1b0e5d4473b54ba612b05ac1
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2017-11-02 12:14:25 -0600

    Dockerfile and sites_enabled changes

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ set -e
 
 STAMP='default'
 SERVER_VERSION='1.3.0'
-CTRACKS_VERSION='0.1.3'
-LIBRARY_VERSION='0.9.4'
+CTRACKS_VERSION='0.1.8'
+# LIBRARY_VERSION='0.9.4' # this will be specified in the CTRACKS_LIBRARY
 
 usage() {
   echo "USAGE: $0 -w WORKERS [-s STAMP] [-l]" >&2
@@ -32,13 +32,12 @@ fi
 set -o verbose # Keep this after the usage message to reduce clutter.
 
 # When development settles down, consider going back to static Dockerfile.
-perl -pne "s/<SERVER_VERSION>/$SERVER_VERSION/g; s/<WEBSITE_VERSION>/$WEBSITE_VERSION/g; s/<LIBRARY_VERSION>/$LIBRARY_VERSION/g" \
+perl -pne "s/<SERVER_VERSION>/$SERVER_VERSION/g; s/<CTRACKS_VERSION>/$CTRACKS_VERSION/g" \
           web-context/Dockerfile.template > web-context/Dockerfile
 
-REPO=gehlenborglab/higlass
-docker pull $REPO # Defaults to "latest", but just speeds up the build, so precise version doesn't matter.
-docker build --cache-from $REPO \
-             --build-arg WORKERS=$WORKERS \
+REPO=visdesignlab/ctracks
+# docker pull $REPO # Defaults to "latest", but just speeds up the build, so precise version doesn't matter.
+docker build --build-arg WORKERS=$WORKERS \
              --tag image-$STAMP \
              web-context
 
