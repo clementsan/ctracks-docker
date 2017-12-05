22927ca6c8cce1c4e1fc1a5288471a57a8bb1c0f
Clement Vachet <cvachet@sci.utah.edu>
2017-12-05 14:10:42 -0700

    Bump ctracks version number

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -e
 
 STAMP='default'
 SERVER_VERSION='1.4.1'
-CTRACKS_VERSION='0.1.13'
+CTRACKS_VERSION='0.1.14'
 # LIBRARY_VERSION='0.9.4' # this will be specified in the CTRACKS_LIBRARY
 
 usage() {
